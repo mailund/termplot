a831ecd21aea200f873e59f64a5ce8e2c30b56fe
Thomas Mailund <tm@kvantify.dk>
2024-05-23 09:23:25 +0200

    plot using a colour key

--- plot.py
+++ plot.py
@@ -1,9 +1,11 @@
 import argparse
 import sys
+from enum import unique
 from functools import partial
 from typing import Iterator
 
 import uniplot
+from xxlimited import Str
 
 plot = partial(uniplot.plot, height=15, width=80)
 
@@ -14,22 +16,52 @@ def skip_comments(inp: Iterator[str]) -> Iterator[str]:
             yield line
 
 
-def load_data(fnames: list[str]) -> Iterator[dict[str, list[float]]]:
+def load_data(fnames: list[str]) -> Iterator[dict[str, list[str]]]:
     for fname in fnames:
         with open(fname) as inp:
             lines = skip_comments(inp)
             header = [h.strip() for h in next(lines).split(",")]
-            cols: list[list[float]] = [[] for _ in header]
+            cols: list[list[Str()]] = [[] for _ in header]
             for line in lines:
                 for d, v in zip(cols, line.split(",")):
-                    d.append(float(v))
+                    d.append(v.strip())
 
-        yield {h: v for h, v in zip(header, cols)}
+        data = {h: v for h, v in zip(header, cols)}
+        assert len(cols) > 0
+        data["__file__"] = [fname] * len(cols[0])
+        yield data
 
 
-def plot_data(keys: list[str], data: list[dict[str, list[float]]]) -> None:
+def merge_data(data: Iterator[dict[str, list[float]]]) -> dict[str, list[float]]:
+    merged = {}
+    for d in data:
+        for k, v in d.items():
+            if k not in merged:
+                merged[k] = v
+            else:
+                merged[k].extend(v)
+    return merged
+
+
+def split(keys: list[str], values: list[float]) -> list[list[float]]:
+    unique_keys = set(keys)
+    split_values = []
+    for key in unique_keys:
+        split_values.append([float(v) for k, v in zip(keys, values) if k == key])
+    return split_values
+
+
+def plot_data(
+    keys: list[str], data: dict[str, list[float]], col: str | None = None
+) -> None:
     for key in keys:
-        plot([d[key] for d in data], title=key, lines=True, color=True)
+        vals = (
+            [float(v) for v in data[key]]
+            if col is None
+            else split(data[col], data[key])
+        )
+        print(len(vals), len(vals[0]))
+        plot(vals, title=key, lines=True, color=True)
 
 
 if __name__ == "__main__":
@@ -43,5 +75,5 @@ if __name__ == "__main__":
         sys.exit(1)
 
     keys = [key.strip() for key in args.keys.split(",")]
-    data = list(load_data([f.name for f in args.input]))
-    plot_data(keys, data)
+    data = merge_data(load_data([f.name for f in args.input]))
+    plot_data(keys, data, "sample")
